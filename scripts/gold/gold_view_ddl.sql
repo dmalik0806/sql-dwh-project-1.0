796cfb0e2ba60c58cea6f61f248fd9dec05fdee9
dmalik0806 <dhruv.malik.dm@outlook.com>
2026-03-31 18:32:14 +0530

    Update gold_view_ddl.sql

--- scripts/gold/gold_view_ddl.sql
+++ scripts/gold/gold_view_ddl.sql
@@ -1,3 +1,18 @@
+/*
+=====================================================================================================
+DDL Script: Create Gold views
+=====================================================================================================
+Usage:
+This script will create views for the Gold layer in the data warehouse.
+The Gold layer represents the final dimension and fact tables (Star schema)
+
+Each view performs transformations and combines data from silver layer to produce
+a clean, enriched and business ready dataset
+
+These views can be used directly for analytics and reporting
+=====================================================================================================
+*/
+
 create view gold.dim_customer as
 select row_number() over(order by a.cst_id) as customer_key
 ,a.cst_id customer_id
@@ -28,3 +43,17 @@ select row_number() over(order by a.prd_key) as product_key
 from silver.crm_prd_info a
 left join silver.erp_px_cat_g1v2 b on a.cat_id=b.id
 where prd_end_dt = '9999-12-31';
+
+create view gold.fact_sales as
+select a.sls_ord_num sales_order_number
+,c.product_key
+,b.customer_key
+,a.sls_order_dt order_date
+,a.sls_ship_dt shipping_date
+,a.sls_due_dt due_date
+,a.sls_sales sales_amount
+,a.sls_quantity quantity
+,a.sls_price price
+from silver.crm_sales_details a
+left join gold.dim_customer b on a.sls_cust_id = b.customer_id
+left join gold.dim_products c on a.sls_prd_key = c.product_number
